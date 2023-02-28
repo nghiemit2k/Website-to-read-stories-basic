636f095cf62aa6a0d36941f13b30db642af64fa5
nghiem <nghiem066@gmail.com>
2023-02-28 11:17:57 +0700

    Init Project

---
+++ truyen.js
@@ -0,0 +1,128 @@
+var listTruyen = [
+    {
+        link: 'KnY.html',
+        Ten: 'Kimestu no Yaiba',
+        Tacgia: 'Gotouge Koyoharu',
+        Tag: 'Shounen',
+        TomTat: 'Tanjiro là con cả trong một gia đình không còn người cha. Một hôm, Tanjiro đi bán than ở một ngôi làng nọ và phải ở lại đó qua đêm vì có tin đồn rằng, về đêm, luôn có một con quỷ lởn vởn ở ngọn núi gần đó. Khi Tanjiro đi về nhà vào ngày hôm sau, bi kịch đang chờ đón cậu.',
+    },
+    {
+        link: 'Conan.html',
+        Ten: 'Thám tử lừng danh Conan',
+        Tacgia: 'Gosho Aoyama',
+        Tag: 'Shounen',
+        TomTat: 'Nhân vật chính của truyện là một thám tử học sinh trung học có tên là Kudo Shinichi, người đã bị biến thành một cậu bé cỡ học sinh tiểu học và luôn cố gắng truy tìm tung tích tổ chức Áo Đen nhằm lấy lại hình dáng cũ.',
+    },
+    {
+        link: 'JK.html',
+        Ten: 'Jusutsu Kaishen',
+        Tacgia: 'Akutami Gege',
+        Tag: 'Shounen, Action',
+        TomTat: 'Yuuji Itadori là một thiên tài có tốc độ và sức mạnh, nhưng cậu ấy muốn dành thời gian của mình trong Câu lạc bộ Tâm Linh. Một ngày sau cái chết của ông mình, anh gặp Megumi Fushiguro, người đang tìm kiếm vật thể bị nguyền rủa mà các thành viên CLB đã tìm thấy. Đối mặt với những con quái vật khủng khiếp bị "Ám", Yuuji nuốt vật thể bị phong ấn để có được sức mạnh của nó và cứu bạn bè của mình! Nhưng giờ Yuuji là người bị "Ám", và cậu ấy sẽ bị kéo vào thế giới ma quỷ ly kỳ của Megumi và những con quái vật độc ác ...',
+    },
+    {
+        link:'',
+        Ten: 'Noblesse',
+        Tacgia: 'Lee Gwang Su, Son Jae Ho',
+        Tag: 'Mangwa',
+        TomTat: 'Sau 820 năm chìm trong giấc ngủ dài, Cadis Etrama Di Raizel(hay Rai) đã thức tỉnh. Rai là một Vampire và vì đã ngủ quá lâu nên chẳng biết gì về thế giới bên ngoài. Sau khi thức tỉnh anh ta đã hòa mình vào cùng những học sinh khác, có nhiều bạn bè. Nhưng có một tổ chức luôn nghiên cứu và thí nghiệm trên cơ thể người mong muốn có được anh làm mẫu vật. Vì vậy mà Rai và các bạn đều bị đặt trong một tình huống vô cùng nguy hiểm...',
+    },
+    {
+        link: 'images/MTMOO/0.jpg',
+        Ten: 'Ma tôn muốn ôm ôm',
+        Tacgia: 'Si Vũ',
+        Tag: 'Manghua, Đam mỹ',
+        TomTat: '',
+    },
+    {
+        link: 'images/OP/1.jpg',
+        Ten: 'One Piece',
+        Tacgia: 'Eiichiro Oda',
+        Tag: 'Action, Adventure, Shounen',
+        TomTat: 'One Piece là câu truyện kể về Luffy và các thuyền viên của mình. Khi còn nhỏ, Luffy ước mơ trở thành Vua Hải Tặc. Cuộc sống của cậu bé thay đổi khi cậu vô tình có được sức mạnh có thể co dãn như cao su, nhưng đổi lại, cậu không bao giờ có thể bơi được nữa. Giờ đây, Luffy cùng những người bạn hải tặc của mình ra khơi tìm kiếm kho báu One Piece, kho báu vĩ đại nhất trên thế giới. Trong One Piece, mỗi nhân vật trong đều mang một nét cá tính đặc sắc kết hợp cùng các tình huống kịch tính, lối dẫn truyện hấp dẫn chứa đầy các bước ngoặt bất ngờ và cũng vô cùng hài hước đã biến One Piece trở thành một trong những bộ truyện nổi tiếng nhất không thể bỏ qua. Hãy đọc One Piece để hòa mình vào một thế giới của những hải tặc rộng lớn, đầy màu sắc, sống động và thú vị, cùng đắm chìm với những nhân vật yêu tự do, trên hành trình đi tìm ước mơ của mình.',
+    },
+    {
+        link: 'images/BC/bia.jpg',
+        Ten: 'Black Clover',
+        Tacgia: 'Tabata Yuuki',
+        Tag: 'Action, Shounen',
+        TomTat: 'Aster và Yuno là hai đứa trẻ bị bỏ rơi ở nhà thờ và cùng nhau lớn lên tại đó. Khi còn nhỏ, chúng đã hứa với nhau xem ai sẽ trở thành Ma pháp vương tiếp theo. Thế nhưng, khi cả hai lớn lên, mọi sô chuyện đã thay đổi. Yuno là thiên tài ma pháp với sức mạnh tuyệt đỉnh trong khi Aster lại không thể sử dụng ma pháp và cố gắng bù đắp bằng thể lực. Khi cả hai được nhận sách phép vào tuổi 15, Yuno đã được ban cuốn sách phép cỏ bốn bá (trong khi đa số là cỏ ba lá) mà Aster lại không có cuốn nào. Tuy nhiên, khi Yuno bị đe dọa, sự thật về sức mạnh của Aster đã được giải mã- cậu ta được ban cuốn sách phép cỏ năm lá, cuốn sách phá ma thuật màu đen. Bấy giờ, hai người bạn trẻ đang hướng ra thế giới, cùng chung mục tiêu',
+    },
+    {
+        link: 'images/LHAS/bia.jpg',
+        Ten: 'Ansatsu Kyoushitsu',
+        Tacgia: 'Matsui Yuusei',
+        Tag: 'Action, Shounen',
+        TomTat: '“Ta là người đã phá huỷ mặt trăng” “Hãy ám sát ta nếu các ngươi muốn cứu lấy trái đất” Một con bạch tuộc chẳng biết tự lỗ nào chui lên cứ luôn miệng gào thét hai câu này và đòi làm thầy giáo của đám học sinh cá biệt lớp 3-E. Chẳng hiểu hắn đang nghĩ cái quái gì khi bản thân không muốn chết nhưng cứ thích khuyến khích người ta ám sát mình. Để coi cái lớp học sát thủ này rồi sẽ đi về đâu...',
+    },
+    {
+        link: 'images/Naruto/bia.jpg',
+        Ten: 'Naruto',
+        Tacgia: 'Kishimoto Masashi',
+        Tag: 'Adventure, Shounen',
+        TomTat: 'Bối cảnh Naruto xảy ra vào mười hai năm trước khi câu chuyện chính thức bắt đầu, một con hồ ly chín đuôi đã tấn công Konohagakure. Nó là một con quái vật có sức mạnh khủng khiếp, chỉ một cái vẫy từ một trong chín cái đuôi của nó có thể tạo ra những cơn sóng thần và san bằng nhiều ngọn núi. Nó gây ra sự hỗn loạn và giết chết rất nhiều người cho đến khi người đứng đầu làng Lá – Hokage đệ tứ – đã đánh bại nó bằng cách đổi lấy mạng sống của mình để phong ấn nó vào trong người một đứa trẻ mới sinh. Đứa trẻ đó tên là Uzumaki Naruto. Bộ truyện kể về cuộc hành trình đầy gian khổ với vô vàn khó khăn, thử thách của Naruto từ khi còn là một cậu bé tới khi trở thành một trong những nhẫn giả vĩ đại nhất. Không chỉ mô tả về một thế giới nhẫn giả huyền bí, Naruto còn mang trong nó nhiều ý nghĩa nhân sinh sâu sắc về tình bạn, tình đồng đội, tình yêu, ước mơ và hi vọng.',
+    },
+    {
+        link: 'images/TBATE/bia.jpg',
+        Ten: 'The Beginning After the End',
+        Tacgia: 'Đang cập nhật',
+        Tag: 'Webtoon, Isekai',
+        TomTat: 'King Grey là người sở hữu tất cả trong một thế giới bị Võ thuật chi phối. Nhưng đi đôi với một sức mạnh to lớn lại là sự cô độc không hồi kết. Thế nên, mặc dù vẻ bên ngoài là một quốc vương mạnh mẽ, nhưng sâu trong thâm tâm lại là một kẻ yếu đuối không có ý chí. Nhưng sau đó lại được tái sinh trong một thế giới fantasy để làm lại một cuộc đời mới. Tuy nhuyên đâu dễ ăn đến thế ? Đằng sau sự hòa bình của thế giới này có vẻ tồn tại một mối đe dọa khủng khiếp nào đó. Với trọng trách lớn lao đó, lý do anh main nhà ta chuyển sinh đến đây là gì?',
+    },
+    {
+        link: 'images/THDT/bia.jpg',
+        Ten: 'Nanatsu no Taizai',
+        Tacgia: 'Suzuki Nakaba',
+        Tag: 'Adventure, Shounen',
+        TomTat: '“Thất đại ác nhân”, một nhóm chiến binh có tham vọng lật đổ vương quốc Britannia, được cho là đã bị tiêu diệt bởi các “hiệp sĩ thánh chiến” mặc dù vẫn còn 1 số người cho rằng họ vẫn còn sống. 10 năm sau, Các hiệp sĩ thánh chiến đã làm 1 cuộc đảo chính và khống chế đức vua, họ trở thành người cai trị độc tài mới của vương quốc. Elizabeth, con gái duy nhất của nhà vua, đã lên đường tìm “thất đại ác nhân” để nhờ họ tái chiếm lại vương quốc. Công chúa có thành công trong việc tìm kiếm “thất đại ác nhân”, các “hiệp sĩ thánh chiến” sẽ làm gì để ngăn chăn cô?'
+    },
+    {
+        link: 'images/DHAT/bia.jpg',
+        Ten: 'Koe No Katachi Special Book',
+        Tacgia: 'Yoshitoki Oima',
+        Tag: 'One shot',
+        TomTat: 'Ngoại truyện của bộ manga cũng như bộ phim cùng tên đến từ Kyoto Animation.',
+    },
+];
+
+function mota(){
+    document.writeln(listTruyen.Ten);
+    document.writeln("Tác giả: " + listTruyen.Tacgia);
+    document.writeln("Thể loại: " + listTruyen.Tag);
+    document.writeln("Tóm tắt: ");
+    document.writeln(listTruyen.TomTat);
+};
+
+var input = document.getElementById('search');
+function Chuanhoa(string){
+    var text = string.trim();
+    text = text.toLowerCase();
+    return text;
+};
+function Search(){
+    var search = Chuanhoa(input);
+    var ten = Chuanhoa(listTruyen.Ten);
+    var i;
+    for (i=0 ; i<listTruyen.length ; i++){
+        if (search == ten)
+            return listTruyen.link;
+    }
+};
+$(document).ready(function() {
+    $('#signup-form').click(function(){
+        var username = $(#namereg).val();
+        alert("Đăng kí thành công!");
+        $('#signup').hide();
+        $('#login').show(function(){
+            $(this).text = username;
+        })
+    });
+    $('#search').on('keyup', function(event) {
+       $(listTruyen.forEach()).filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(tukhoa)>-1);
+       });
+    });
+    $('img').mouseOver(function(){
+        ('.decription').show();
+    });
+ });
